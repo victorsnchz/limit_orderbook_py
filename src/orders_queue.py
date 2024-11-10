b4898dcce3ba062506800684e7f7e2b4b7e76e13
Victor Sanchez <thespielmaster@MacBookPro.mynet>
2024-11-10 11:04:11 +0000

    fix order overfill, base implementation order queue for given price lvl level

---
+++ src/orders_queue.py
@@ -0,0 +1,33 @@
+import collections
+from order import Order
+
+class OrdersQueue:
+
+    def __init__(self):
+        self.queue = collections.OrderedDict()
+    
+    def add_order(self, order: Order):
+        self.queue[order.id] = order
+
+    def remove_order(self, order_id: str) -> Order:
+        self.queue.pop(order_id)
+
+    def is_empty(self):
+        return not bool(self.queue)
+
+    def match_order(self, order_to_match: Order) -> list[Order]:
+
+        filled_orders = []
+
+        while( (not order_to_match.is_filled()) and (not self.is_empty())):
+            
+            waiting_order_id = next(iter(self.queue))
+            waiting_order_quantity_to_fill = self.queue[waiting_order_id].remaining_quantity
+            self.queue[waiting_order_id].fill_quantity(order_to_match.remaining_quantity)
+            order_to_match.fill_quantity(waiting_order_quantity_to_fill - self.queue[waiting_order_id].remaining_quantity)
+
+            if self.queue[waiting_order_id].is_filled():
+                filled_orders.append(self.queue.pop(waiting_order_id))
+            
+        return filled_orders
+
---
+++ test/test_orders_queue.py
@@ -0,0 +1,53 @@
+import unittest
+unittest.TestLoader.sortTestMethodsUsing = None
+
+from src.orders_queue import OrdersQueue
+from order import Order
+from custom_types import OrderExecutionRules, OrderType, BookSide
+
+class  TestOrdersQueue(unittest.TestCase):
+
+    orders_queue = OrdersQueue()
+
+
+    def test_case_add(self):
+
+        orders_queue = OrdersQueue()
+
+        new_order = gtc_order = Order(type = OrderType.LIMIT, execution_rules = OrderExecutionRules.GOOD_TILL_CANCELLED,
+                          side = BookSide.BID, initial_quantity=100.0, price = 100.0)
+        orders_queue.add_order(new_order)
+
+        self.assertEqual(new_order.id in orders_queue.queue, True)
+
+    def test_case_remove(self):
+
+        orders_queue = OrdersQueue()
+        new_order = gtc_order = Order(type = OrderType.LIMIT, execution_rules = OrderExecutionRules.GOOD_TILL_CANCELLED,
+                          side = BookSide.BID, initial_quantity=100.0, price = 100.0)
+        orders_queue.add_order(new_order)
+        order_id = list(orders_queue.queue.keys())[0]
+        orders_queue.remove_order(order_id)
+
+        self.assertEqual(bool(orders_queue.queue), False)
+
+    def test_case_match_order(self):
+
+        orders_queue = OrdersQueue()
+
+        first_in_order = gtc_order = Order(type = OrderType.LIMIT, execution_rules = OrderExecutionRules.GOOD_TILL_CANCELLED,
+                          side = BookSide.BID, initial_quantity=100.0, price = 100.0)
+        
+        last_in_order = gtc_order = Order(type = OrderType.LIMIT, execution_rules = OrderExecutionRules.GOOD_TILL_CANCELLED,
+                          side = BookSide.BID, initial_quantity=100.0, price = 100.0)
+        
+        orders_queue.add_order(first_in_order)
+        orders_queue.add_order(last_in_order)
+
+        order_to_match = Order(type = OrderType.LIMIT, execution_rules = OrderExecutionRules.GOOD_TILL_CANCELLED,
+                          side = BookSide.ASK, initial_quantity=150.0, price = 100.0)
+        
+        filled = orders_queue.match_order(order_to_match)
+        
+        self.assertEqual(filled[0], (first_in_order))
+        
