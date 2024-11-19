716739174c0de1a77a74f88181a4002e5f197a16
Victor Sanchez <thespielmaster@MacBookPro.mynet>
2024-11-19 20:58:33 +0000

    add conversion trade side to book side

--- src/custom_types.py
+++ src/custom_types.py
@@ -1,18 +1,26 @@
 import enum
 
-class BookSide(enum.Enum):
-    BID = 1
-    ASK = -1
-
-    
 class TradeSide(enum.Enum):
     BUY = 1
     SELL = -1
 
+class BookSide(enum.Enum):
+    BID = 1
+    ASK = -1
+
 class OrderType(enum.Enum):
     MARKET = enum.auto()
-    LIMIT = enum.auto()
+    LIMIT = enum.auto() 
 
 class OrderExecutionRules(enum.Enum):
     FILL_OR_KILL = 'fill_or_kill'
     GOOD_TILL_CANCELLED ='good_till_cancelled'
+
+
+# TODO
+# fix, this looks sketchy
+def trade_side_to_book_side(trade_side: TradeSide) -> BookSide.value:
+    if TradeSide.value == 'BUY':
+        return BookSide.BID
+    
+    return BookSide.ASK
