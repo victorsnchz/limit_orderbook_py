edede6ae0b9aa607fa240866d07f5c1a6440c39b
Victor Sanchez <thespielmaster@MacBookPro.mynet>
2024-11-19 20:11:18 +0000

    tree skeleton instead of heap

--- src/heaps.py
+++ src/heaps.py
@@ -1,16 +1,5 @@
 import dataclasses
 
 
-# good case for inheritance?
-# or just brute code both?
-
-class MinHeap:
-
-    def __init__(self):
-        pass
-
-
-class MaxHeap:
-
-    def __init__(self):
-        pass
+# TODO
+# SELF REBALANCING TREE
