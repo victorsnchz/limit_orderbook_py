a3634a5256b5bdbbfe19fa8ae6bd34e6341d5d2c
Victor Sanchez <thespielmaster@MacBook-Pro-de-Victor.local>
2024-11-23 11:37:22 +0000

    add tests, debug

--- test/test_orderbook_levels.py
+++ test/test_orderbook_levels.py
@@ -1,16 +1,45 @@
 import unittest
-from orders_queue import OrdersQueue
-from order import Order
-from custom_types import TradeSide, OrderExecutionRules, BookSide
+unittest.TestLoader.sortTestMethodsUsing = None
 
-class TestBookSide(unittest.TestCase):
+from price_levels import PriceLevels
+from order import Order
+from custom_types import OrderType,OrderExecutionRules, BookSide
+
+
+
+class TestPriceLevels(unittest.TestCase):
     
-    queue = OrdersQueue()
+    price_levels = PriceLevels(BookSide.ASK)
+    order = Order(type =  OrderType.LIMIT, execution_rules= OrderExecutionRules.GOOD_TILL_CANCELLED, 
+                      side = BookSide.ASK, initial_quantity=100, price = 100)
     
     def test_case_add_order(self):
-        order = Order(TradeSide.BUY, execution_rules= OrderExecutionRules.FILL_OR_KILL, 
+        
+        self.price_levels.post_order(self.order)
+
+        self.assertEqual(self.order.id in self.price_levels.levels[self.order.price].queue, True)
+
+
+    def test_case_add_order(self):
+        order = Order(type =  OrderType.LIMIT, execution_rules= OrderExecutionRules.GOOD_TILL_CANCELLED, 
                       side = BookSide.ASK, initial_quantity=100, price = 100)
         
-        self.queue.add_order(order)
+        self.price_levels.post_order(order)
 
-        self.assertEqual(order.id in self.queue.levels, True)
+        self.assertEqual(order.id in self.price_levels.levels[order.price].queue, True)
+
+    def test_case_top_of_book(self):
+        order2 = Order(type =  OrderType.LIMIT, execution_rules= OrderExecutionRules.GOOD_TILL_CANCELLED, 
+                      side = BookSide.ASK, initial_quantity=99, price = 100)
+        
+        self.price_levels.post_order(order2)
+
+        self.assertEqual(self.price_levels.top_of_book(), order2.price)
+
+    def test_case_can_match(self):
+        self.assertEqual(self.price_levels.can_match(100), True)
+
+    def test_case_cancel_order(self):
+        
+        self.price_levels.cancel_order(self.order)
+        self.assertEqual(self.price_levels.can_match(self.order.price), False)
