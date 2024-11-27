08b363f710c67d6eea716c175ae74c8dc61ef638
Victor Sanchez <thespielmaster@MacBookPro.mynet>
2024-11-27 10:34:11 +0000

    orderbook post_order and test

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from order import Order
 from orders_queue import OrdersQueue
 from price_levels import PriceLevels
+from orderbook import OrderBook
 from custom_types import BookSide, OrderExecutionRules, OrderType
 
 def main():
@@ -8,16 +9,9 @@ def main():
     first_in_order = Order(type = OrderType.LIMIT, execution_rules = OrderExecutionRules.GOOD_TILL_CANCELLED,
                           side = BookSide.BID, initial_quantity=100.0, price = 100.0)
     
-    last_in_order = Order(type = OrderType.LIMIT, execution_rules = OrderExecutionRules.GOOD_TILL_CANCELLED,
-                          side = BookSide.BID, initial_quantity=100.0, price = 100.0)
-    
-    queue = OrdersQueue()
-    levels = PriceLevels(BookSide.BID)
+    orderbook = OrderBook()
 
-    levels.post_order(first_in_order)
-
-
-    levels.cancel_order(first_in_order)
+    orderbook.post_order(first_in_order)
 
     print('hello world')
 
--- src/orderbook.py
+++ src/orderbook.py
@@ -1,22 +1,65 @@
 from order import Order
-from custom_types import BookSide, TradeSide
+from custom_types import BookSide, OrderExecutionRules, OrderType
+from price_levels import PriceLevels
 
 class OrderBook:
 
     def __init__(self):
         
        #todo 
-        self.bids = None # min heap
-        self.asks = None # max heap
+        self.bids = PriceLevels(BookSide.BID) # make it a tree
+        self.asks = PriceLevels(BookSide.ASK) # make it a tree
 
     def match_order(self, order_in: Order):
         pass
 
-    def cancel_order(self, id):
-        pass
+    def post_order(self, order: Order):
 
-    def can_match_order(self, order: Order):
-        return True
+        if self.can_match_order(order):
+            self.match_order(order)
+        
+        conditionA = order.execution_rules != OrderExecutionRules.FILL_OR_KILL
+        conditionB = order.type == OrderType.LIMIT
+        conditionC = order.remaining_quantity > 0
+
+        if conditionA and conditionB and conditionC:
+            self.add_order_to_book(order)
+
+    def add_order_to_book(self, order: Order) -> None:
+        if order.side == BookSide.BID:
+            self.bids.post_order(order)
+
+        elif order.side == BookSide.ASK:
+            self.asks.post_order(order)
+        
+        else:
+            raise ValueError(f'order side {order.side} is not valid')
+
+    def cancel_order(self, order: Order):
+        if order.side == BookSide.BID:
+            self.bids.cancel_order(order)
+        
+        elif order.side == BookSide.ASK:
+            self.asks.cancel_order(order)
+
+        else:
+            raise ValueError(f'order side {order.side} is not valid')
+
+    def can_match_order(self, order: Order) -> bool:
+
+        if order.side == BookSide.BID:
+            if self.bids.is_empty():
+                return False
+            return order.price >= self.asks.top_of_book()
+
+        elif order.side == BookSide.ASK:
+            if self.asks.is_empty():
+                return False
+            return order.price <= self.bids.top_of_book()
+        
+        else:
+            raise ValueError(f'order side {order.side} is not valid')
+
 
 
     def modify_order(self, order_id) -> int:
