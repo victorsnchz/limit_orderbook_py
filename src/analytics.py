6e316e2e733401732846c55c64a58502a9879e6c
Victor Sanchez <thespielmaster@MacBook-Pro-de-Victor.local>
2025-01-09 16:55:51 +0100

    save orderbook states

---
+++ src/analytics.py
@@ -0,0 +1,10 @@
+from orderbook import OrderBook
+from price_levels import PriceLevels
+
+
+class Analytics:
+    def __init__(self):
+        pass
+
+
+    
--- src/orderbook.py
+++ src/orderbook.py
@@ -5,9 +5,8 @@ import execution_schedules
 
 class OrderBook:
 
-    def __init__(self):
+    def __init__(self):     
         
-       #todo 
         self.bids = Bids()
         self.asks = Asks()
 
@@ -30,6 +29,7 @@ class OrderBook:
         raise TypeError(f'invalid type {type(side)}, must be of type Side')
     
     def get_bid_ask_mid(self) -> tuple[float, float, float]:
+        
         if not self.bids.is_empty() and not self.asks.is_empty():
             top_bid = self.bids.get_best_price()
             top_ask = self.asks.get_best_price()
@@ -39,5 +39,10 @@ class OrderBook:
         mid = .5 * (top_bid + top_ask)
 
         return top_bid, top_ask, mid
-
     
+    def get_orderbook_state(self) -> tuple[dict, dict]:
+
+        bids_state = self.bids.get_price_levels_state()
+        asks_state = self.asks.get_price_levels_state()
+
+        return bids_state, asks_state
