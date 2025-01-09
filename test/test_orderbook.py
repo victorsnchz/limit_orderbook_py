ab99653caf75cb8accbfb0421ae9318dc7ac8808
Victor Sanchez <thespielmaster@MacBook-Pro-de-Victor.local>
2025-01-09 11:28:06 +0100

    add test bid-ask-mid

--- test/test_orderbook.py
+++ test/test_orderbook.py
@@ -7,6 +7,7 @@ from orderbook import OrderBook
 from order import Order, LimitOrder, OrderID, OrderParameters
 from custom_types import OrderType, Side, ExecutionRules
 from price_levels import Bids, Asks
+from order_execution import LimitOrderExecution
 
 # blend of unit-testing and integration testing
 
@@ -29,6 +30,25 @@ class TestOrderbook(unittest.TestCase):
         side = orderbook.get_opposite_side_levels(side_to_get)
 
         self.assertEqual(type(side), Asks)
+
+    def test_case_get_bid_ask_mid(self):
+
+        orderbook = OrderBook()
+        
+        order = LimitOrder(OrderParameters(Side.BID, 100), OrderID(0),
+                               limit_price=100, execution_rules = ExecutionRules.GTC)
+        order2 = LimitOrder(OrderParameters(Side.ASK, 100), OrderID(1),
+                               limit_price=101, execution_rules = ExecutionRules.GTC)
+
+        exec = LimitOrderExecution(order, orderbook)
+        exec.post_order()
+
+        exec = LimitOrderExecution(order2, orderbook)
+        exec.post_order()
+
+        bid, ask, mid = orderbook.get_bid_ask_mid()
+
+        self.assertEqual((bid, ask, mid), (100, 101, 100.5))
     
 
 if __name__ == '__main__':
