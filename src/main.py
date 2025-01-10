52ce0b1c8ecdf54593b19b46053c4a6ae29dd5c8
Victor Sanchez <thespielmasterspam@icloud.com>
2025-01-10 14:56:47 +0100

    save orderbook states to csv

--- src/main.py
+++ src/main.py
@@ -3,19 +3,34 @@ from orderbook import OrderBook
 from custom_types import Side, ExecutionRules, OrderType
 from price_levels import Bids, Asks
 from order_execution import LimitOrderExecution
+from saver import Saver
 
 def main():
 
     orderbook = OrderBook()
-    order_to_post = LimitOrder(OrderParameters(Side.BID, 100), OrderID(0),
+
+    bid1 = LimitOrder(OrderParameters(Side.BID, 100), OrderID(0),
                             limit_price=100, execution_rules = ExecutionRules.GTC)
-    
-    exec = LimitOrderExecution(order_to_post, orderbook)
-    exec.post_order()
+    bid2 = LimitOrder(OrderParameters(Side.BID, 200), OrderID(1),
+                            limit_price=100, execution_rules = ExecutionRules.GTC)
+    bid3 = LimitOrder(OrderParameters(Side.BID, 100), OrderID(0),
+                            limit_price=105, execution_rules = ExecutionRules.GTC)
 
-    order_side = order_to_post.get_side()
+    ask1 = LimitOrder(OrderParameters(Side.ASK, 100), OrderID(2),
+                            limit_price=110, execution_rules = ExecutionRules.GTC)
+    ask2 = LimitOrder(OrderParameters(Side.ASK, 100), OrderID(3),
+                            limit_price=120, execution_rules = ExecutionRules.GTC)
 
-    pass
+    orders = [bid1, bid2, bid3, ask1, ask2]
+
+    for order in orders:
+        exec = LimitOrderExecution(order, orderbook)
+        exec.execute()
+
+    saver = Saver()
+    saver.orderbook_state_to_csv(orderbook)
+
+    saver.top_of_book_state_to_csv(orderbook)
 
 if __name__ == '__main__':
     main()
