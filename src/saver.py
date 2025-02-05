96ce7cb83c9146b3d6614468eec86d1037394cbf
Victor Sanchez <thespielmaster@MacBook-Pro-de-Victor.local>
2025-02-05 09:49:59 +0100

    re organize file paths for saver and tests

--- src/saver.py
+++ src/saver.py
@@ -5,55 +5,65 @@ from files_manager import write_dict_to_csv
 import os
 import datetime
 import csv
+import pathlib
 
 class Saver:
 
-    def __init__(self, data_directory: str = f'../data'):
+    def __init__(self, data_directory: str = '../data'):
         
         self._data_directory = data_directory
+        self._current = pathlib.Path(__file__).parent.absolute()
         self.date = datetime.datetime.today()
 
     def order_to_csv(self, order: Order, path: str = None) -> None:
         
         pass
 
-    def orderbook_state_to_csv(self, orderbook: OrderBook, path: str = 'orderbook_states') -> None:        
+    def orderbook_state_to_csv(self, orderbook: OrderBook, path: str = None) -> None:        
 
         date = datetime.datetime.now().date().strftime('%Y_%m_%d')
-        dir = f'{self._data_directory}/{path}/{date}' if path is not None else f'{self._data_directory}/{date}'
-
-        if not os.path.exists(dir):
-            os.makedirs(dir)
+        
+        if path is None:
+            orderbook_state_dir = f'{self._data_directory}/orderbook_state_dir/{date}' 
+        else:
+            orderbook_state_dir = f'{self._data_directory}/{path}'
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+        if not os.path.exists(orderbook_state_dir):
+            os.makedirs(orderbook_state_dir)
 
         bids_state, asks_state = orderbook.get_orderbook_state()
         timestamp = datetime.datetime.now().time().strftime('%H_%M_%S')
 
-        with open(f'{dir}/bid_{timestamp}.csv', 'w') as csv_file:
+        with open(f'{orderbook_state_dir}/bid_{timestamp}.csv', 'w') as csv_file:
             
             writer = csv.writer(csv_file)
             write_dict_to_csv(writer, bids_state)
 
-        with open(f'{dir}/ask_{timestamp}.csv', 'w') as csv_file:
+        with open(f'{orderbook_state_dir}/ask_{timestamp}.csv', 'w') as csv_file:
             
             writer = csv.writer(csv_file)
             write_dict_to_csv(writer, asks_state)
 
-    def top_of_book_state_to_csv(self, orderbook: OrderBook, path: str = 'top_of_book_states') -> None:        
+    def top_of_book_state_to_csv(self, orderbook: OrderBook, path: str = None) -> None:        
 
         date = datetime.datetime.now().date().strftime('%Y_%m_%d')
-        dir = f'{self._data_directory}/{path}/{date}' if path else f'{self._data_directory}/{date}'
+
+        if path is None:
+            top_of_book_states_dir = f'{self._data_directory}/top_of_book_states/{date}' 
+        else:
+            top_of_book_states_dir = f'{self._data_directory}/{path}' 
         
-        if not os.path.exists(dir):
-            os.makedirs(dir)
+        if not os.path.exists(top_of_book_states_dir):
+            os.makedirs(top_of_book_states_dir)
 
         top_bid_state, top_ask_state = orderbook.get_top_of_book_state()
         timestamp = datetime.datetime.now().time().strftime('%H_%M_%S')
-        with open(f'{dir}/bid_{timestamp}.csv', 'w') as csv_file:
+        with open(f'{top_of_book_states_dir}/bid_{timestamp}.csv', 'w') as csv_file:
             
             writer = csv.writer(csv_file)
             write_dict_to_csv(writer, top_bid_state)
         
-        with open(f'{dir}/ask_{timestamp}.csv', 'w') as csv_file:
+        with open(f'{top_of_book_states_dir}/ask_{timestamp}.csv', 'w') as csv_file:
             
             writer = csv.writer(csv_file)
             write_dict_to_csv(writer, top_ask_state)
--- test/helper.py
+++ test/helper.py
@@ -2,6 +2,29 @@ import csv
 import pathlib
 import os
 
+import datetime
+
+def get_results_names_bid_ask(data_dir: str, test_case: str, test_name: str) -> tuple[str, str]:
+    
+    timestamp = datetime.datetime.now().time().strftime('%H_%M_%S')
+
+    results_data_directory = f'{data_dir}/{test_case}/{test_name}/results'
+
+    bid = f'{results_data_directory}/bid_{timestamp}'
+    ask = f'{results_data_directory}/ask_{timestamp}'
+
+    return bid, ask
+
+def get_target_names_bid_ask(data_dir: str, test_case: str, test_name: str) -> tuple[str, str]:
+    
+    targets_data_directory = f'{data_dir}/{test_case}/{test_name}/targets'
+
+    bid = f'{targets_data_directory}/bid'
+    ask = f'{targets_data_directory}/ask'
+
+    return bid, ask
+
+
 def read_csv_file(target_dir: str, file_name: str):
 
     path = f'{target_dir}/{file_name}/'
@@ -14,7 +37,7 @@ def read_csv_file(target_dir: str, file_name: str):
 
 def read_two_csvs(csv_file_A: str, csv_file_B: str, strict = True):
 
-    with open(f'{csv_file_A}.csv') as csv_A, open(f'{csv_file_B}.csv') as csv_B:
+    with open(csv_file_A) as csv_A, open(csv_file_B) as csv_B:
         
         reader_A = csv.reader(csv_A)
         reader_B = csv.reader(csv_B)
