681519d44eeda700353da148b1308d8c5b9a89ba
Victor Sanchez <thespielmaster@macbookpro.home>
2026-04-05 21:46:31 +0200

    Revert "fix(order_id_generator): Lock() instead of lock()"
    This reverts commit 5e678cfba4ab2679bf6798847ccbf536a7e5ef68.

--- src/orders/order_id_generator.py
+++ src/orders/order_id_generator.py
@@ -4,7 +4,7 @@ import threading
 class OrderIdGenerator:
     def __init__(self, start: int = 0):
         self._counter = start
-        self._lock = threading.Lock()
+        self._lock = threading.lock()
 
     def next_id(self) -> int:
         with self._lock:
