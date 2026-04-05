af2c4cb033662cd73a5f4c482582701248157649
Victor Sanchez <thespielmasterspam@icloud.com>
2026-04-05 22:53:28 +0200

    refector(factory): rewrite against API, inject generator

--- src/orders/factory.py
+++ src/orders/factory.py
@@ -1,45 +1,53 @@
 # product = order
 # concrete = limit / market orders
 
-from orders.order import Order, LimitOrder, MarketOrder, OrderParameters, OrderID
+from orders.order import Order, OrderID, OrderSpec
 from abc import ABC, abstractmethod
-from bookkeeping.custom_types import OrderType, Side, ExecutionRules
+from bookkeeping.custom_types import OrderType, Side, ExecutionRule
+from src.orders.order_id_generator import OrderIdGenerator
+
 
 class OrderFactory(ABC):
-
-    def __init__(self):
-        pass
+    def __init__(self, generator: OrderIdGenerator):
+        self._generator = generator
 
     @abstractmethod
-    def create_order(self, side: Side, initial_quantity: int,
-                     user_id: int, **kwargs) -> Order:
+    def create_order(self, side: Side, quantity: int, user_id: int, **kwargs) -> Order:
         pass
-        
+
+
 class LimitOrderFactory(OrderFactory):
-    def __init__(self):
-        pass
+    def __init__(self, generator: OrderIdGenerator):
+        super().__init__(generator)
 
-    def create_order(self, side: Side, initial_quantity: int, user_id: int, 
-                     limit_price: float, execution_rule: ExecutionRules = None) -> Order:
-        
-        order_parameters = OrderParameters(side, initial_quantity)
-        order_id = OrderID(user_id)
+    def create_order(
+        self,
+        side: Side,
+        quantity: int,
+        user_id: int,
+        limit_price: float,
+        execution_rule: ExecutionRule,
+    ) -> Order:
 
-        return LimitOrder(order_parameters, order_id, limit_price, execution_rule)
+        spec = OrderSpec(side, OrderType.LIMIT, quantity, limit_price, execution_rule)
+        id_ = OrderID(self._generator.next_id(), user_id)
+
+        return Order(spec, id_)
+
 
 class MarketOrderFactory(OrderFactory):
-    def __init__(self):
-        pass
+    def __init__(self, generator: OrderIdGenerator):
+        super().__init__(generator)
 
-    def create_order(self, side: Side, initial_quantity: int, user_id: int) -> Order:
-        
-        order_parameters = OrderParameters(side, initial_quantity)
-        order_id = OrderID(user_id)
-        
-        return MarketOrder(order_parameters, order_id)
-    
+    def create_order(self, side: Side, quantity: int, user_id: int) -> Order:
+
+        spec = OrderSpec(side, OrderType.MARKET, quantity)
+        id_ = OrderID(self._generator.next_id(), user_id)
+
+        return Order(spec, id_)
+
 
 map_type_to_factory = {
-        OrderType.LIMIT: LimitOrderFactory,
-        OrderType.MARKET: MarketOrderFactory
-    }
+    OrderType.LIMIT: LimitOrderFactory,
+    OrderType.MARKET: MarketOrderFactory,
+}
