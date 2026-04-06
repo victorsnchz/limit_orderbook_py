ca955742822132d6c8d811dc4ab657d9449ac1a6
Victor Sanchez <thespielmaster@macbookpro.home>
2026-04-06 23:37:27 +0200

    refactor(book_side): abstract base, levels naming, best_price and top_level as properties

--- src/orderbook/book_side.py
+++ src/orderbook/book_side.py
@@ -1,10 +1,11 @@
-from orderbook.orders_queue import OrdersQueue
-from orders.order import Order
-
+from src.orderbook.orders_queue import OrdersQueue
+from src.orders.order import Order
+from src.bookkeeping.custom_types import LevelState
 from sortedcontainers import SortedDict
+from abc import ABC, abstractmethod
 
-class BookSide:
 
+class BookSide(ABC):
     """
     Store order queues in a tree map.
     O(1) access to top of book (likely most accessed book layer).
@@ -12,32 +13,36 @@ class BookSide:
     """
 
     def __init__(self):
-        self.price_map = SortedDict()
-    
+        self.levels = SortedDict()
+
     @property
     def is_empty(self) -> bool:
         """
         Check if level is empty.
         """
-        return not bool(self.price_map)
+        return not bool(self.levels)
 
-    def post_order(self, order: Order ) -> None:
+    def post_order(self, order: Order) -> None:
         """
         Add order to queue at approriate price level. If no level exists create it.
         """
 
-        if order.limit_price not in self.price_map:
-            self.price_map[order.limit_price] = OrdersQueue()
+        if order.limit_price not in self.levels:
+            self.levels[order.limit_price] = OrdersQueue()
 
-        self.price_map[order.limit_price].add_order(order)
+        self.levels[order.limit_price].add_order(order)
 
-    def get_best_price(self) -> int:
+    @property
+    @abstractmethod
+    def best_price(self) -> int:
         """
         Return top-of-book price.
         """
         pass
-    
-    def get_top_of_book(self) -> OrdersQueue:
+
+    @property
+    @abstractmethod
+    def top_level(self) -> OrdersQueue:
         """
         Return top-of-book queue.
         """
@@ -47,49 +52,61 @@ class BookSide:
         """
         Check if there exists an order queue at input price.
         """
-        return self.price_map[price].is_empty()
-    
+        return self.levels[price].is_empty
+
     def delete_level(self, price: float):
         """
         Delete queue at given price.
         """
-        del self.price_map[price]
+        del self.levels[price]
 
-    def get_states(self) -> dict[float, tuple[float, int]]:
+    def get_states(self) -> dict[int, LevelState]:
         """
-        Return state for all price levels: {price: (volume, #participants)}
+        Return state for all price levels
         """
+        states = {}
 
-        price_map_info = {}
-
-        for price, queue in self.price_map.items():
+        for price, queue in self.levels.items():
             total_volume = 0
+            order_count = 0
             participants = set()
 
             for order in queue.queue.values():
-                participants.add(order.user_id)
                 total_volume += order.remaining_quantity
+                order_count += 1
+                participants.add(order.user_id)
 
-            price_map_info[price] = (total_volume, len(participants))
+            states[price] = LevelState(
+                total_volume=total_volume,
+                order_count=order_count,
+                participant_count=len(participants),
+            )
 
-        return price_map_info
-    
-    def get_top_state(self) -> dict[float, tuple[int, int]]:
+        return states
+
+    def get_top_state(self) -> dict[int, LevelState]:
         """
         Return state for top-of-book ONLY: {price: (total_volume, #participants)}
         """
-        best_price = self.get_best_price()
-        top_of_book = self.get_top_queue()
+        price = self.best_price
+        queue = self.top_level
 
         total_volume = 0
+        order_count = 0
         participants = set()
 
-
-        for order in top_of_book.queue.values():
+        for order in queue.queue.values():
             total_volume += order.remaining_quantity
+            order_count += 1
             participants.add(order.user_id)
 
-        return {best_price: (total_volume, len(participants))}
+        return {
+            price: LevelState(
+                total_volume=total_volume,
+                order_count=order_count,
+                participant_count=len(participants),
+            )
+        }
 
     def get_volumes(self) -> dict[float, int]:
         """
@@ -98,39 +115,43 @@ class BookSide:
 
         volumes = SortedDict()
 
-        for price, queue in self.price_map.items():
+        for price, queue in self.levels.items():
             volume = 0
 
             for order in queue.queue.values():
                 volume += order.remaining_quantity
-        
+
             volumes[price] = volume
-        
+
         return volumes
 
-class BidSide(BookSide):
 
+class BidSide(BookSide):
     """
     Bids prices sorted from highest-to-lowest. Stored in SortedDicts as asks BUT returns order are inverted.
     """
 
-    def get_best_price(self) -> float:
-        return self.price_map.keys()[-1]
-    
-    def get_top_queue(self) -> OrdersQueue:
-        return self.price_map.values()[-1]
-    
+    @property
+    def best_price(self) -> int:
+        return self.levels.keys()[-1]
+
+    @property
+    def top_level(self) -> OrdersQueue:
+        return self.levels.values()[-1]
+
     def get_volumes(self):
         volumes = super().get_volumes()
         return dict(reversed(volumes.items()))
 
-class AskSide(BookSide):
 
+class AskSide(BookSide):
     def __init__(self):
         super().__init__()
 
-    def get_best_price(self) -> float:
-        return self.price_map.keys()[0]
-    
-    def get_top_queue(self) -> OrdersQueue:
-        return self.price_map.values()[0]
+    @property
+    def best_price(self) -> int:
+        return self.levels.keys()[0]
+
+    @property
+    def top_level(self) -> OrdersQueue:
+        return self.levels.values()[0]
