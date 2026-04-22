28ca96f942eb60ea82e9c87f704a698f526522ba
Victor Sanchez <thespielmaster@macbookpro.home>
2026-04-22 11:50:56 +0200

    chore: remove dead code, incomplete skeletons, deprecated calls

--- src/simulation/agents/retail_agent.py
+++
@@ -1,8 +0,0 @@
-from random_agent import RandomAgent
-
-class RetailAgent(RandomAgent):
-    
-    def __init__(self, orderbook = None, bankroll = 0):
-        super().__init__(orderbook, bankroll)
-
-    
